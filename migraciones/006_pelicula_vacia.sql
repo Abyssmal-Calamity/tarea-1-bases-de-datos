b5032856820cd99debc568a31162eecbef6f8dd0
Abyssmal-Calamity <thomascalhoun.chanique@alumnos.ulagos.cl>
2026-06-18 17:54:23 -0400

    eliminado archivo basura

--- migraciones/006_pelicula_vacia.sql
+++
@@ -1,8 +0,0 @@
-SET search_path TO test;
--- 5. Películas sin ventas
-SELECT p.titulo
-FROM pelicula p
-LEFT JOIN funcion f USING (pelicula_id)
-LEFT JOIN entrada e USING (funcion_id)
-WHERE e.entrada_id IS NULL;
-INSERT INTO schema_migrations VALUES ('006_pelicula_vacia');
